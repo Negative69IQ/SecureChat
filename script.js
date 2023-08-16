3f37dcdbc50463f8010d9160b21eab2b918789fb
Negative69IQ <142377199+Negative69IQ@users.noreply.github.com>
2023-08-16 01:51:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket('ws://localhost:3000'); // Connect to WebSocket server
+const socket = new WebSocket('nodejs-serverless-function-express-five-omega.vercel.app'); // Connect to WebSocket server
 
 const messageInput = document.getElementById('message-input');
 const sendButton = document.getElementById('send-button');
